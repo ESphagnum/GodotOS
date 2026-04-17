4f323d998d76f45ec18ee6aeb0e067287c68d3a6
ESphagnum <sfagnum14@gmail.com>
2026-04-17 22:23:56 +0300

    update WindowWrapper

--- core/window_wrapper.gd
+++ core/window_wrapper.gd
@@ -3,14 +3,18 @@ extends Control
 var dragging = false
 var offset = Vector2()
 
-func _on_header_panel_gui_input(event: InputEvent) -> void:
+func _on_header_panel_gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			dragging = event.pressed
-			offset = get_global_mouse_position() - get_parent().global_position
+			offset = get_global_mouse_position() - global_position
 			
 			if dragging:
-				get_parent().move_to_front()
+				move_to_front()
 
 	if event is InputEventMouseMotion and dragging:
-		get_parent().global_position = get_global_mouse_position() - offset
+		global_position = get_global_mouse_position() - offset
+
+
+func _on_close_button_pressed() -> void:
+	queue_free()
