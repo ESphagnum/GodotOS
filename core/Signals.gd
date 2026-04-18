ae9059c68320d2f266d3b9232a2d80421bbe8f38
ESphagnum <sfagnum14@gmail.com>
2026-04-18 07:43:45 +0300

    add global Signals.gd

---
+++ core/Signals.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# Сигналы системы
+signal start_menu_toggled(is_active: bool) # Для кнопки "Пуск"
+signal request_app_launch(scene_path: String, app_name: String) # Для запуска программ
+
+# Сигналы окон (для связи окон с Таскбаром)
+signal window_opened(window_ref, title)
+signal window_closed(window_ref)
+signal window_focused(window_ref)
+
+# Сигналы системных событий
+signal notification_sent(message: String, type: String) # Для всплывающих уведомлений
