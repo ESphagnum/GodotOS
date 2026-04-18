8164fce4277fe93722bf9de54ecaa3d45f7ed8bc
ESphagnum <sfagnum14@gmail.com>
2026-04-18 10:29:29 +0300

    hotfix for AppConfig

--- core/AppConfig.gd
+++ core/AppConfig.gd
@@ -3,6 +3,7 @@ class_name AppConfig
 
 @export var app_name: String = "New App"
 @export var icon: Texture2D
+@export_file("*.tscn") var scene_path: String 
 @export var show_in_taskbar: bool = true
 @export var show_in_tray: bool = false
 @export var start_minimized: bool = false
