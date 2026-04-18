7cee4dc907f35c581be49a411fd4be20e0d2b4c3
ESphagnum <sfagnum14@gmail.com>
2026-04-18 08:28:22 +0300

    update global Signals.gd

--- core/Signals.gd
+++ core/Signals.gd
@@ -2,10 +2,11 @@ extends Node
 
 # Сигналы системы
 signal start_menu_toggled(is_active: bool) # Для кнопки "Пуск"
-signal request_app_launch(scene_path: String, app_name: String) # Для запуска программ
+signal request_app_launch(config: AppConfig)
+
 
 # Сигналы окон (для связи окон с Таскбаром)
-signal window_opened(window_ref, title)
+signal window_opened(window_ref: Control, title: String)
 signal window_closed(window_ref)
 signal window_focused(window_ref)
 
