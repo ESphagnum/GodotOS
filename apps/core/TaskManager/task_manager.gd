9578b69e91db9136046a6c298a27438946a2901d
ESphagnum <sfagnum14@gmail.com>
2026-04-18 10:29:04 +0300

    add FocusButton for TaskManager

--- apps/core/TaskManager/task_manager.gd
+++ apps/core/TaskManager/task_manager.gd
@@ -1,33 +1,45 @@
 extends BaseApp
 
-@onready var process_list = $VBoxContainer/ProcessList
-@export var app_config : AppConfig 
+@onready var item_list = $VBoxContainer/ProcessList
 
 func _ready():
+	super._ready() # Инициализируем связь с окном через BaseApp
+	
+	# Подключаемся к глобальным сигналам, чтобы список обновлялся сам
+	Signals.window_opened.connect(func(_w, _t): refresh_list())
+	Signals.window_closed.connect(func(_w): refresh_list())
+	Signals.window_focused.connect(func(_w): refresh_list())
+	
 	refresh_list()
-	# Обновляем список каждые 2 секунды, чтобы видеть изменения
-	var timer = get_tree().create_timer(2.0)
-	timer.timeout.connect(_on_timer_timeout)
-
-func _on_timer_timeout():
-	refresh_list()
-	get_tree().create_timer(2.0).timeout.connect(_on_timer_timeout)
 
 func refresh_list():
-	process_list.clear()
-	# Берем массив окон напрямую из нашего синглтона
+	item_list.clear()
+	
 	for window in WindowManager.windows:
 		if is_instance_valid(window) and window.config:
-			var icon = window.config.icon
-			var idx = process_list.add_item(window.config.app_name, icon)
-			# Сохраняем ссылку на объект окна в метаданные строки
-			process_list.set_item_metadata(idx, window)
+			var app_name = window.config.app_name
+			
+			# Добавляем пометку, если окно активно
+			if WindowManager.active_window == window:
+				app_name += " (Активно)"
+			
+			var idx = item_list.add_item(app_name, window.config.icon)
+			item_list.set_item_metadata(idx, window)
 
-func _on_end_task_button_pressed():
-	var selected = process_list.get_selected_items()
+# Функция для кнопки "Перейти" (Focus)
+func _on_focus_button_pressed() -> void:
+	var selected = item_list.get_selected_items()
 	if selected.size() > 0:
-		var window_to_kill = process_list.get_item_metadata(selected[0])
+		var window_to_focus = item_list.get_item_metadata(selected[0])
+		if is_instance_valid(window_to_focus):
+			window_to_focus.visible = true # На случай, если было скрыто
+			WindowManager.set_active_window(window_to_focus)
+
+
+func _on_end_task_button_pressed() -> void:
+	var selected = item_list.get_selected_items()
+	if selected.size() > 0:
+		var window_to_kill = item_list.get_item_metadata(selected[0])
 		if is_instance_valid(window_to_kill):
-			# Вызываем стандартную функцию закрытия окна
+			# Вызываем закрытие через метод обертки
 			window_to_kill._on_close_button_pressed()
-			refresh_list()
